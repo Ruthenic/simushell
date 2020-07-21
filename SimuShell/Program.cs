65447b27afe8c852e0e35863b1e4edab8eed81e4
Ruthenic <53356436+Ruthenic@users.noreply.github.com>
2020-07-20 23:42:08 -0400

    Update Program.cs
    added copyright notice and permission notice

--- SimuShell/Program.cs
+++ SimuShell/Program.cs
@@ -3,8 +3,16 @@ using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using SUCC;
+/*
+Copyright (c) 2019 Charlie Poole, Rob Prouse
 
-
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+*/
 namespace SimuShell
 {
     static class Program
